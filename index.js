ad97169d8fbe94c8fe863d6ca22614f8992596d6
Rooshaan <mirrooshaanrafi@gmail.com>
2023-06-27 18:44:35 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Server } = require('socket.io');
+const { createServer } = require('http');
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.emit('Hi sockets established');
+
+  socket.on('join room', (roomId) => {
+    console.log('socket room *' + roomId + '* created...');
+    socket.join(roomId);
+  });
+
+  //emit recevied message to specified room
+  socket.on('sending signal', (payload) => {
+    console.log('sending signal coming:' + payload);
+    io.to(payload.room).emit('received signal', payload.signal);
+  });
+
+  socket.on('returning signal', (payload) => {
+    console.log('receiving signal coming:' + payload);
+    io.to(payload.room).emit('returned signal', payload.signal);
+  });
+});
+
+io.listen(3000);
+console.log('socket io server started......');
